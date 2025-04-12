444a9d3f1b66b277f961539fa1d5decccf280416
Serhii Vykaliuk <serhii.v@gmail.com>
2025-04-12 17:38:27 +0300

    Update PostgreSQL user and privileges in Docker setup; change password and modify role creation in start_docker.sh and set_privileges.sql

--- hw3/docker/set_privileges.sql
+++ hw3/docker/set_privileges.sql
@@ -7,9 +7,9 @@ BEGIN
     SELECT nspname FROM pg_namespace
     WHERE nspname NOT LIKE 'pg_%' AND nspname <> 'information_schema'
   LOOP
-    EXECUTE format('GRANT USAGE ON SCHEMA %I TO migration_user;', schema_name);
-    EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA %I TO migration_user;', schema_name);
-    EXECUTE format('GRANT SELECT ON ALL SEQUENCES IN SCHEMA %I TO migration_user;', schema_name);
+    EXECUTE format('GRANT USAGE ON SCHEMA %I TO migrant;', schema_name);
+    EXECUTE format('GRANT SELECT ON ALL TABLES IN SCHEMA %I TO migrant;', schema_name);
+    EXECUTE format('GRANT SELECT ON ALL SEQUENCES IN SCHEMA %I TO migrant;', schema_name);
   END LOOP;
 END
 $$;
@@ -17,4 +17,4 @@ $$;
 -- Grant pglogical access
 CREATE EXTENSION IF NOT EXISTS pglogical;
 GRANT USAGE ON SCHEMA pglogical TO PUBLIC;
-GRANT SELECT ON ALL TABLES IN SCHEMA pglogical TO migration_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA pglogical TO migrant;
--- hw3/docker/start_docker.sh
+++ hw3/docker/start_docker.sh
@@ -1,9 +1,22 @@
 export POSTGRES_PASSWORD="it[y.MS6m0Eb9pK"
 
-docker compose up -d --force-recreate
 
+#####
+cat Dockerfile 
+
+
+#####
+cat docker-compose.yml
+sleep 2
+
+
+##### run docker
+docker compose up -d --force-recreate
+docker ps
 sleep 5
 
+
+# check pglogical settings
 psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "
 SELECT name, setting, unit, source, boot_val, reset_val
 FROM pg_settings
@@ -15,26 +28,37 @@ WHERE name IN (
   'max_wal_senders',
   'max_worker_processes'
 );"
+sleep 2
 
 
-wget https://raw.githubusercontent.com/neondatabase/postgres-sample-dbs/main/periodic_table.sql
+# load database
+wget -q https://raw.githubusercontent.com/neondatabase/postgres-sample-dbs/main/periodic_table.sql
+
 psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "CREATE DATABASE periodic_table;"
+
 psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/periodic_table" -f periodic_table.sql
-psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/periodic_table" -c 'select pt."AtomicNumber", pt."Element", pt."Symbol"  from periodic_table pt where pt."AtomicMass" < 10;'
+
 rm periodic_table.sql
+sleep 2
 
 
-psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "CREATE ROLE migration_user WITH LOGIN PASSWORD '${POSTGRES_PASSWORD}';"
-psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "ALTER ROLE migration_user WITH LOGIN;"
-psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "ALTER ROLE migration_user WITH REPLICATION;"
-psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "CREATE DATABASE migration_user WITH OWNER migration_user;"
+# create user for migration
+psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "CREATE ROLE migrant WITH LOGIN PASSWORD '${POSTGRES_PASSWORD}';"
+
+psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "ALTER ROLE migrant WITH LOGIN;"
+
+psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "ALTER ROLE migrant WITH REPLICATION;"
+
+psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/postgres" -c "CREATE DATABASE migrant WITH OWNER migrant;"
+sleep 2
 
 
-DBS=("migration_user" "postgres" "periodic_table")
-
+DBS=("migrant" "postgres" "periodic_table")
 for DB in "${DBS[@]}"; do
   psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/$DB" -f set_privileges.sql
 done
+sleep 2
 
 
-# gcloud beta resource-config bulk-export --project=de-module-3 --resource-format=terraform >> gcp_resources.tf
+# check database load
+psql -d "postgres://postgres:${POSTGRES_PASSWORD}@localhost/periodic_table" -c 'select pt."AtomicNumber", pt."Element", pt."Symbol"  from periodic_table pt where pt."AtomicMass" < 10;'
